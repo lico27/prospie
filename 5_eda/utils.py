bcdc3caefc94b0c08f7571057ffe83acea4ae27e
Liam <liam@liamco.io>
2025-11-02 16:03:36 +0000

    refactor funders dataframe builder

--- 5_eda/utils.py
+++ 5_eda/utils.py
@@ -45,4 +45,13 @@ def get_table_from_supabase(url, key, table_name, batch_size=1000, delay=0.2, fi
     return df
 
 def add_gbp_columns(x):
+    """
+    Adds a new column with currency formatted in GBP.
+    """
     return f"£{x:,.2f}" if pd.notnull(x) else ""
+
+def explode_lists(df, col):
+    """
+    Explodes a list column to create one row per list item.
+    """
+    return df.explode(col).dropna(subset=[col])
