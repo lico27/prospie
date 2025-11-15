b35dc975eba9dd688243aa2c9ff396e6abb14045
Liam <liam@liamco.io>
2025-11-15 20:07:48 +0000

    finish funder word count analysis

--- 6_eda/eda_utils.py
+++ 6_eda/eda_utils.py
@@ -11,3 +11,12 @@ def explode_lists(df, col):
     Explodes a list column to create one row per list item.
     """
     return df.explode(col).dropna(subset=[col])
+
+def check_word_counts(df, col, n=10):
+    """
+    Gets longest and shortest registered_nums for a word count column.
+    """
+    df_no_nas = df.dropna(subset=[col])
+    longest = df_no_nas.sort_values(col, ascending=False).head(n)["registered_num"].tolist()
+    shortest = df_no_nas.sort_values(col, ascending=True).head(n)["registered_num"].tolist()
+    return longest, shortest
