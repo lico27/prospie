bad8685f1892c92421330621a57a571cba2d4333
Liam <liam@liamco.io>
2025-11-19 18:37:07 +0000

    start making evaluation form

---
+++ x_evaluation/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="evaluation-section">
+      <div className="evaluation-content">
+        <h1>Thank you for agreeing to evaluate prospie!</h1>
+        <div className="card">
+          <p>
+            The form will display here when it's ready.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
