d0154687cb7958f4e18c9c5b80473aed893bab6b
Liam <liam@liamco.io>
2025-11-21 18:24:27 +0000

    make app landing page

--- x_evaluation/src/App.jsx
+++ x_evaluation/src/App.jsx
@@ -1,12 +1,24 @@
-import './App.css'
-import EvaluationForm from './components/EvaluationForm'
+import { useState } from "react"
+import "./App.css"
+import EvaluationForm from "./components/EvaluationForm"
 
 function App() {
+  const [showForm, setShowForm] = useState(false)
+
   return (
     <div className="evaluation-section">
       <div className="evaluation-content">
-        <h1>Thank you for agreeing to evaluate prospie!</h1>
-        <EvaluationForm />
+        {!showForm ? (
+          <>
+            <h1>Thank you for agreeing to evaluate prospie!</h1>
+            <div className="card">
+              <p>Instructions will go here</p>
+            </div>
+            <button onClick={() => setShowForm(true)}>Start</button>
+          </>
+        ) : (
+          <EvaluationForm />
+        )}
       </div>
     </div>
   )
